71a267d422f1c7844a915778ed6bc9f515ae9d21
c34809368 <berraavci@std.iyte.edu.tr>
2020-10-19 09:38:34 +0000

    I wrote hypotenus, i fixed it

--- lab2/example5.py
+++ lab2/example5.py
@@ -1,4 +1,4 @@
 per_1=float(input("please enter the first edge:"))
 per_2=float(input("please enter the second edge:"))
-hypotenus=((per_1 ** 2)+(per_2 ** 2))**(1/2)
-print("hypotenus of the triangle is:" , float(hypotenus))
+hypotenuse=((per_1 ** 2)+(per_2 ** 2))**(1/2)
+print("hypotenuse of the triangle is:" , float(hypotenuse))
