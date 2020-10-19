a254f385c1f50a021cf47c0c52d59b9ca8e44090
c34809368 <berraavci@std.iyte.edu.tr>
2020-10-19 09:16:07 +0000

    .

--- lab2/example3.py
+++ lab2/example3.py
@@ -2,6 +2,6 @@ a=2
 b=6
 c=-20
 delta=(b**2)-(4*a*c)
-root_1=(b+(delta**(1/2)))/(2*a)
-root_2=(b-(delta**(1/2)))/(2*a)
+root_1=(-b+(delta**(1/2)))/(2*a)
+root_2=(-b-(delta**(1/2)))/(2*a)
 print("The roots are:",root_1,root_2)
