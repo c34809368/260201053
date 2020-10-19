76ca1c1d16d8ae3356fde3a9f747bbe0ce723017
c34809368 <berraavci@std.iyte.edu.tr>
2020-10-19 09:36:59 +0000

    .

---
+++ lab2/example5.py
@@ -0,0 +1,4 @@
+per_1=float(input("please enter the first edge:"))
+per_2=float(input("please enter the second edge:"))
+hypotenus=((per_1 ** 2)+(per_2 ** 2))**(1/2)
+print("hypotenus of the triangle is:" , float(hypotenus))
