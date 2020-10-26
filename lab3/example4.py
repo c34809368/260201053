ae48d40e805925ab87a97a3c46379596fb3e96cd
c34809368 <berraavci@std.iyte.edu.tr>
2020-10-26 09:18:57 +0000

    I changed the equal sign's place as you explained in the lesson.

--- lab3/example4.py
+++ lab3/example4.py
@@ -1,7 +1,7 @@
 age=int(input("please enter an age:"))
-if age<=6 or age>=60:
+if age<6 or age>60:
     print("you are free of price.")
-elif age>6 and age<18:
+elif age>=6 and age<=18:
     print("the price you should pay is: 1.5 tl")
 else:
     print("the price you should pay is: 3 tl")
