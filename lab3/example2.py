a43c7884141c7ef65dd4c4c5bb5467924e3dd465
c34809368 <berraavci@std.iyte.edu.tr>
2020-10-26 08:43:15 +0000

    .

---
+++ lab3/example2.py
@@ -0,0 +1,12 @@
+n_1=input("please enter the first number:")
+n_2=input("please enter the second number:")
+n_3=input("please enter the third number:")
+
+if n_1<n_2 and n_1<n_3:
+    print("the minimum number is:", n_1)
+elif n_2<n_3 and n_2<n_3:
+    print("the minimum number is:", n_2)
+else:
+    print("the minimum number is:", n_3)
+
+
