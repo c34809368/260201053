5e4cacc5bed221bf08ec96210260df25a39aa534
c34809368 <berraavci@std.iyte.edu.tr>
2020-10-27 12:19:22 +0000

    .

--- lab3/example5.py
+++ lab3/example5.py
@@ -1,1 +1,22 @@
-month=str(input("please enter the month:"))
+month=input("please enter the month:")
+day=int(input("please enter the day the day: "))
+
+if (month=='january') or (month=='february') or (month=='march'):
+	season='winter'
+elif (month=='april') or (month=='may') or (month=='june'):
+	season='spring'
+elif (month=='july') or (month=='august') or (month=='september'):
+	season='summer'
+else:
+	season='autumn'
+
+if (month=='march') and (day>=20):
+	season='spring'
+elif (month=='june') and (day>=21):
+	season='summer'
+elif (month=='september') and (day>=22):
+	season='autumn'
+elif (month=='december') and (day>=21):
+	season='winter'
+
+print("the date is:",day,month,"and the season is:",season)
