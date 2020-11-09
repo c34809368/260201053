049c7439972a9a2447b249efe281146300b76c02
c34809368 <berraavci@std.iyte.edu.tr>
2020-11-09 09:20:56 +0000

    .

--- lab4/example1.py
+++ lab4/example1.py
@@ -1,7 +1,5 @@
-number=input("please enter a number:")
-d_1=number[:-2]
-d_2=number[:-1]
-digits=d_1 + d_2
+number=list(input("please enter a number:"))
+digits=number[-1] + number[-2:]
 print("sum of the last two digits is:",digits)
 
 
---
+++ lab4/example4.py
@@ -0,0 +1,6 @@
+a=int(input("please enter an integer:"))
+b=int(input("please enter an integer:"))
+power=1
+for i in range(1,b+1):
+  power*=a
+print(power)
