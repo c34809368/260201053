6249d1a8cb47e420b7a8c8b260a8855a429b370e
c34809368 <berraavci@std.iyte.edu.tr>
2020-11-16 09:14:12 +0000

    simplified as you recommended

--- lab5/example2.py
+++ lab5/example2.py
@@ -1,9 +1,7 @@
 N=int(input("How many numbers you would like to enter?"))
 count=0
-for i in range(1,N+1):
+for _ in range(N):
     m=int(input("please enter an integer:"))
     if m%2==0:
         count+=1
-    else:
-        count=count
 print("percentage of even numbers is:",float(count/N*100),"%")
