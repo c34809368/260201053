2ebeb0e33d0c25e461f890d06bd6429144e61111
c34809368 <berraavci@std.iyte.edu.tr>
2020-11-23 15:46:24 +0000

    .

--- lab6/example4.py
+++ lab6/example4.py
@@ -1,8 +1,8 @@
-item=input("please enter items of your list seperated by space:")
-liste=item.split()
-setOfListe=set(liste)
+items=list(map(int, input("please enter items of your list seperated by space:").split()))
+setOfListe=set(items)
 newListe=[]
 for element in setOfListe:
   newListe.append(element)
   newListe.sort(reverse=True)
 print(newListe)
+#I had to use map because 009 was reading as less than 09 and I couldn't find any better solution for now#
---
+++ lab6/example5.py
@@ -0,0 +1,5 @@
+n=int(input("please enter a number:"))
+identityMatrix=[]
+for i in range(n):
+  for j in range(i):
+    print(0)
