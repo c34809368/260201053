75675b276e045ef4d29146cb5fcbbe4858342d65
c34809368 <berraavci@std.iyte.edu.tr>
2020-12-01 18:49:58 +0000

    .

--- lab6/example1.py
+++ lab6/example1.py
@@ -2,6 +2,16 @@ email="ceng113@example.com"
 email=email.lower()
 user=input("please enter the e-mail:")
 user=user.lower()
+newuser=user.split("@")
+beforeat,afterat=newuser[0],newuser[1]
+x=str(newuser[0])
+x.replace(".","")
+lastuser=x+str(afterat)
+if lastuser==email:
+  print("True email!")
+else:
+  print("Wrong email!")
+
 
 
 
--- lab6/example3.py
+++ lab6/example3.py
@@ -2,14 +2,15 @@ studentnumber=int(input("please enter how many students there are in this class:
 grades=[]
 averageGrades=[]
 for i in range(studentnumber):
-  grades=input("enter midterm-1, midterm-1 and final seperated with comma").split(",")
-  grades[0]=midterm1
-  grades[1]=midterm2
-  grades[3]=final
+  grades=input("enter midterm-1, midterm-1 and final seperated with comma:").split(",")
+  midterm1=int(grades[0])
+  midterm2=int(grades[1])
+  final=int(grades[2])
   average=(midterm1*0.3)+(midterm2*0.3)+(final*0.4)
   averageGrades.append(average)
 print("average grades of the students are:",averageGrades)
 A_students=0
-if averageGrades>90:
-  A_students+=1
+for average in averageGrades:
+  if average>90:
+    A_students+=1
 print("there are",A_students,"AA students in this class.")
