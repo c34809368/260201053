e3c6dc8082584605f8fff2df517f81ff41140df6
c34809368 <berraavci@std.iyte.edu.tr>
2020-12-27 20:48:35 +0000

    .

--- lab9/example2.py
+++ lab9/example2.py
@@ -2,4 +2,7 @@ def get_reversed(liste):
   if len(liste)==0:
     return []
   else:
-    return(get_reversed())
+    return(get_reversed(liste[1:]+[liste[0]]))
+    
+liste=[1,2,3]
+print(get_reversed(liste))
